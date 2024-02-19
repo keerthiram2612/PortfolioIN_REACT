cf6644da45a5c03781d1ec1b29a7a97931283c4a
Keerthana <keerthiram2612@gmail.com>
2024-02-19 20:30:27 +0530

    creating the router and installing it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
 import "./index.css" ;
+import Home from "./routes/Home"
+import Contact from "./routes/Contact"
+import Project from "./routes/Project"
+import About from "./routes/About"
+import {Routes,Route} from "react-router-dom"
+
 function App() {
   return (
-    <div>
-    </div>
+   <>
+    <Routes>
+      <Route path = "/" element ={<Home/>}/>
+      <Route path = "/contact" element ={<Contact/>}/>
+      <Route path = "/project" element ={<Project/>}/>
+      <Route path = "/about" element ={<About/>}/>
+    </Routes>
+   </>
   );
 }
 
