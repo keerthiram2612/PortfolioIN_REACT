b5e590be9a374a5f56e0582f8330cb63dbcb2b5b
Keerthana <keerthiram2612@gmail.com>
2024-02-26 20:06:15 +0530

    importing the usestate with react Hooks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom"
 import "./Navbar.css"
-import React from 'react'
-import {FaBars} from "react-icons/fa"
+import React, { useState } from 'react'
+import {FaBars, FaTimes} from "react-icons/fa"
+
 
 export default function Navbar() {
+  const [click,setClick]=useState(false);
+  const handleClick = () => setClick(!click);
   return (
     <div className="header">
       <Link to="/">
         <h1>Portfolio</h1>
       </Link>
-      <ul className="nav-menu">
+      <ul className={click?"nav-menu active":"nav-menu"}>
         <li>
             <Link to="/">Home</Link>
         </li>
@@ -23,8 +26,11 @@ export default function Navbar() {
             <Link to="/project">Project</Link>
         </li>
       </ul>
-      <div className="hamburger">
-        <FaBars size ={20} style={{color:"white"}}/>
+      <div className="hamburger" onClick={handleClick}>
+        {click? (<FaTimes size={20} style={{color:"#fff"}}/>):(
+          <FaBars size={20} style={{color:" #fff"}}/>
+        )}
+        
       </div>
     </div>
   )
