327fd3df888925444223e0ce46235f07191e4e36
Keerthana <keerthiram2612@gmail.com>
2024-02-26 20:44:58 +0530

    creating the component for the home section

---
+++ src/components/HeroImage.js
@@ -0,0 +1,22 @@
+import "./HeroImageStyle.css"
+import IntroImg from "../assets/intro-bg.jpg"
+import React from 'react'
+import { Link } from "react-router-dom"
+
+export default function HeroImage() {
+  return (
+    <div className="hero">
+       <div className="mask">
+        <img className="intro-img" src = {IntroImg} alt = "IntroImg"/>
+       </div>
+       <div className="content">
+        <p>HI , I'M A FRONTEND DEVELOPER.</p>
+        <h1>REACT DEVELOPER</h1>
+        <div>
+            <Link to = "/project" className="btn">Projects</Link>
+            <Link to = "/contact" className="btn">Contact</Link>
+        </div>
+       </div>
+    </div>
+  )
+}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import HeroImage from "../components/HeroImage";
 
 export default function Home() {
   return (
     <div>
      <Navbar/>
+     <HeroImage/>
     </div>
   )
 }
