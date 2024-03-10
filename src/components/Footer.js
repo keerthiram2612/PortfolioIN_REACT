feca35e4fa0f28600472105bdcdd817991b6fd06
Keerthana <keerthiram2612@gmail.com>
2024-03-10 20:51:53 +0530

    adding the icons to the footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import { FaHome, FaPhone } from "react-icons/fa"
+import { FaHome, FaPhone ,FaMailBulk, FaFacebook, FaLinkedin, FaInstagram } from "react-icons/fa"
 import "./FooterStyle.css"
 
 import React from 'react'
@@ -16,11 +16,22 @@ export default function Footer() {
                 <p>Chennai ,TamilNadu</p>
             </div>
             <div className="phone">
-                <h4> <FaPhone size={20} style={{color:"white",marginRight:"2rem"}}/>+91 6382819707</h4>
-                
+                <h4><FaPhone size={20} style={{color:"white",marginRight:"2rem"}}/>+91 6382819707</h4>
+            </div>
+            <div className="email">
+            <h4><FaMailBulk size={20} style={{color:"white",marginRight:"2rem"}}/>+91 6382819707</h4>
             </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <h4>About me</h4>
+          <p>This is Keerthana FrontEnd developer .<br/>
+            I enjoy discussing new projects and design <br/>
+            challenges. </p>
+        </div>
+        <div className="social">
+        <FaLinkedin size={30} style={{color:"white",marginRight:"1rem"}}/>
+        <FaInstagram size={30} style={{color:"white",marginRight:"1rem"}}/>
+        </div>
       </div>
     </div>
   )
