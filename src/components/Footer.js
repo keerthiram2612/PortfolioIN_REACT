94d16d1f58343e251fe2e4c9b9f088b6032d8a5d
Keerthana <keerthiram2612@gmail.com>
2024-03-11 20:38:35 +0530

    styling the left and the right of the component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,27 +10,28 @@ export default function Footer() {
         <div className="left">
             <div className="location">
                 <FaHome size={25} style={{color:"white",marginRight:"2rem"}}/>
-            </div>
-            <div>
-                <p>72 A Ambedkar nagar.</p>
-                <p>Chennai ,TamilNadu</p>
+                <p>72 A Ambedkar nagar,</p>
+                <br/>
+                <p>Chennai.</p>
+                
             </div>
             <div className="phone">
                 <h4><FaPhone size={20} style={{color:"white",marginRight:"2rem"}}/>+91 6382819707</h4>
             </div>
             <div className="email">
-            <h4><FaMailBulk size={20} style={{color:"white",marginRight:"2rem"}}/>+91 6382819707</h4>
+            <h4><FaMailBulk size={20} style={{color:"white",marginRight:"2rem"}}/>keerthiram2612@gmail.com</h4>
             </div>
         </div>
         <div className="right">
           <h4>About me</h4>
-          <p>This is Keerthana FrontEnd developer .<br/>
-            I enjoy discussing new projects and design <br/>
+          <p>This is Keerthana FrontEnd developer .
+            I enjoy discussing <br/>
+            new projects and design 
             challenges. </p>
-        </div>
-        <div className="social">
+            <div className="social">
         <FaLinkedin size={30} style={{color:"white",marginRight:"1rem"}}/>
         <FaInstagram size={30} style={{color:"white",marginRight:"1rem"}}/>
+        </div>
         </div>
       </div>
     </div>
