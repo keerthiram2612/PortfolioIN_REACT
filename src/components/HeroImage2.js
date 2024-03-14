5a373501b9a500af390f996ab224b76e9ff34f87
Keerthana <keerthiram2612@gmail.com>
2024-03-14 20:50:57 +0530

    adding the heroimage2 component in the project component

---
+++ src/components/HeroImage2.js
@@ -0,0 +1,14 @@
+import "./HeroImage2Style.css"
+
+import React from 'react'
+
+export default function HeroImage2() {
+  return (
+    <div className="hero-img">
+      <div className="heading">
+        <h1>This a heading</h1>
+        <p>This a text</p>
+      </div>
+    </div>
+  )
+}
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
-
+import HeroImage2 from '../components/HeroImage2'
 export default function Project() {
   return (
     <div>
       <Navbar/>
+      <HeroImage2/>
       <Footer/>
     </div>
   )
