ee3ccfa015f6327ee76d12cef4155df57d795dc3
Keerthana <keerthiram2612@gmail.com>
2024-04-09 20:23:40 +0530

    adding the reference to the icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,18 @@
-import { FaHome, FaPhone ,FaMailBulk, FaFacebook, FaLinkedin, FaInstagram } from "react-icons/fa"
+import { FaHome, FaPhone ,FaMailBulk, FaFacebook, FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa"
 import "./FooterStyle.css"
 
 import React from 'react'
 
 export default function Footer() {
+  const handleClick =()=>{
+    window.location.href="https://www.linkedin.com/in/keerthana-rajaram-66aa33242/"
+  }
+  const handleClick1=()=>{
+    window.location.href = "https://www.instagram.com/keerthana_rajaram/"
+  }
+  const handleClick2=()=>{
+    window.location.href="https://github.com/keerthiram2612"
+  }
   return (
     <div className="footer">
       <div className="footer-container">
@@ -28,8 +37,9 @@ export default function Footer() {
             new projects and design 
             challenges. </p>
             <div className="social">
-        <FaLinkedin size={30} style={{color:"white",marginRight:"1rem"}}/>
-        <FaInstagram size={30} style={{color:"white",marginRight:"1rem"}}/>
+        <FaLinkedin onClick={handleClick}size={30} style={{color:"white",marginRight:"1rem"}}/>
+        <FaInstagram onClick={handleClick1}size={30} style={{color:"white",marginRight:"1rem"}}/>
+        <FaGithub  onClick={handleClick2}size={30} style={{color:"white",marginRight:"1rem"}}/>
         </div>
         </div>
       </div>
